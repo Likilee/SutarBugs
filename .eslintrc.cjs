a21253582914038adc5dbaa93161cec7418e4936
Likilee <gnsdlrl@daum.net>
2023-08-03 12:17:28 +0900

    style(lint): 린트 규칙 Single Quote 로 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,67 +1,59 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "no-use-before-define": ["error", { functions: false }],
-    "linebreak-style": "off",
-    camelcase: "off",
-    "import/no-cycle": "off",
-    "arrow-parens": "off",
-    "symbol-description": "off",
-    "prefer-destructuring": [
-      "error",
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'linebreak-style': 'off',
+    camelcase: 'off',
+    'import/no-cycle': 'off',
+    'arrow-parens': 'off',
+    'symbol-description': 'off',
+    'prefer-destructuring': [
+      'error',
       {
         object: false,
         array: false,
       },
     ],
-    "no-plusplus": "off",
-    "import/prefer-default-export": "off",
-    "import/no-extraneous-dependencies": "off",
-    "no-nested-ternary": "off",
-    "prettier/prettier": [
-      "error",
+    'no-plusplus': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-nested-ternary': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
+        singleQuote: true,
       },
     ],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        ts: "never",
-        "": "never",
-      },
-    ],
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
-    "vue/multi-word-component-names": "off",
-    "vue/html-self-closing": [
-      "error",
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': [
+      'error',
       {
         html: {
-          void: "always",
-          normal: "never",
-          component: "always",
+          void: 'always',
+          normal: 'never',
+          component: 'always',
         },
-        svg: "always",
-        math: "always",
+        svg: 'always',
+        math: 'always',
       },
     ],
-    "import/no-unresolved": "off", // TODO - need to fix alias(@) error
-    "import/no-absolute-path": "off",
+    'import/no-unresolved': 'off', // TODO - need to fix alias(@) error
+    'import/no-absolute-path': 'off',
   },
 };
